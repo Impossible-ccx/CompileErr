8d5ffa91761f6417ec42b2bfde10c3f93271ef2e
impossible.ccx@gmail.com <impossible.ccx@gmail.com>
2024-10-20 21:19:23 +0800

    PQ加入，不会忘了

---
+++ CompileErr/PriorityQ.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CompileErr
+{
+    public class PriorityQueue<T1, T2>
+    {
+        class MyVector
+        {
+            public T1 target;
+            public int value;
+        }
+        private List<MyVector> list = new List<MyVector>();
+        public int Count { get { return list.Count; } }
+        public T1 Dequeue()
+        {
+            MyVector result = null;
+            foreach (MyVector myVector in list)
+            {
+                if (result == null || result.value > myVector.value)
+                {
+                    result = myVector;
+                }
+            }
+            list.Remove(result);
+            return result.target;
+        }
+        public void Enqueue(T1 input, int val)
+        {
+            MyVector newVector = new MyVector();
+            newVector.target = input;
+            newVector.value = val;
+            list.Add(newVector);
+        }
+    }
+}
